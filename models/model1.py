556052e40082a73ae5d5a3678dec23c2e78eab9f
hasibamin95 <57613552+hasibamin95@users.noreply.github.com>
2025-02-14 20:20:57 -0500

    Update model1.py

--- models/model1.py
+++ models/model1.py
@@ -2,8 +2,70 @@ import numpy as np
 import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from models.block import Choice_Block, Choice_Block_x
+
+class VGGBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size):
+        super(VGGBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu1(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu2(out)
+        out = self.maxpool(out)
+        return out
+
+class VGGBlock_Nomax(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size):
+        super(VGGBlock_Nomax, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.relu2 = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu1(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu2(out)
+        return out
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.conv3 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_channels)
+
+    def forward(self, x):
+        residual = x
+        out1 = self.conv1(x)
+        out1 = self.bn1(out1)
+        out1 = self.relu(out1)
+        out1 = self.conv2(out1)
+        out1 = self.bn2(out1)
+        out2 = self.conv3(residual)
+        out2 = self.bn3(out2)
+        out = out1 + out2
+        out = self.relu(out)
+        return out
 
 class VGGBlock(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size):
